02234c4077ebbbe478f36f51a988810dc2890b03
jdidi94 <jdididaoud404@gmail.com>
2021-04-22 09:37:50 +0200

    cards finish

--- react-client/src/components/card.jsx
+++ react-client/src/components/card.jsx
@@ -6,7 +6,14 @@ function PersonnelCard () {
   
       <img className="image-profile" src="https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg"/>
        <div className="personnel-container">
+         <div className="contact_div">
         <h2 className="title-person">Bill Gates</h2>
+        <div className="contact_person">
+      <span className="material-icons">face</span>  
+      <span className="material-icons">face</span>
+        </div>  
+
+        </div>
         <p className="s-paragraph-person">full stack javascript developer</p>
         <p className="x-paragraph-person">hi im jdidi im new one in the feild happy to see you all.start-up called Virtual-Dawn, developing awesome VR games. I'm also responsible for all of my company's web development needs
 My passion is to use technology based solutions, to help solve real world challenges.</p>
--- react-client/src/components/projectCard.jsx
+++ react-client/src/components/projectCard.jsx
@@ -8,21 +8,62 @@ function ProjectCard() {
           className="project_img"
           src="https://image.shutterstock.com/z/stock-photo-responsive-web-design-on-mobile-devices-phone-laptop-and-tablet-pc-288954107.jpg"
         />
-        <div className="tech-items">
+        <div className="tech_its">
           <h3 className="tech_items_tags">CSS</h3>
           <h3  className="tech_items_tags">CSS</h3>
           <h3  className="tech_items_tags">CSS</h3>
         </div>
-        <h3 className="project-title">Tunisian fann</h3>
+        <h3 className="project_title">Tunisian fann</h3>
         <p className="project_paragraph">
           If you’re confused about how to write your meta descriptions, you’re
           not the only one. To help remove that confusion, I’m going to dig into
-          what makes a great meta description and share 15 killer meta
-          description examples.
+          what makes a great meta
         </p>
         <div className="project_button">
-          <button className="teck_button">hellloooooo </button>
-          <button className="teck_button">Button</button>
+          <button className="teck_button_relate">Demo </button>
+          <button className="teck_button_relate">Code</button>
+        </div>
+      </div>
+      <div className="card_prject">
+        <img
+          className="project_img"
+          src="https://image.shutterstock.com/z/stock-photo-responsive-web-design-on-mobile-devices-phone-laptop-and-tablet-pc-288954107.jpg"
+        />
+        <div className="tech_its">
+          <h3 className="tech_items_tags">CSS</h3>
+          <h3  className="tech_items_tags">CSS</h3>
+          <h3  className="tech_items_tags">CSS</h3>
+        </div>
+        <h3 className="project_title">Tunisian fann</h3>
+        <p className="project_paragraph">
+          If you’re confused about how to write your meta descriptions, you’re
+          not the only one. To help remove that confusion, I’m going to dig into
+          what makes a great 
+        </p>
+        <div className="project_button">
+          <button className="teck_button_relate">code </button>
+          <button className="teck_button_relate">Demo</button>
+        </div>
+      </div>
+      <div className="card_prject">
+        <img
+          className="project_img"
+          src="https://image.shutterstock.com/z/stock-photo-responsive-web-design-on-mobile-devices-phone-laptop-and-tablet-pc-288954107.jpg"
+        />
+        <div className="tech_its">
+          <h3 className="tech_items_tags">#CSS</h3>
+          <h3  className="tech_items_tags">#CSS</h3>
+          <h3  className="tech_items_tags">#CSS</h3>
+        </div>
+        <h3 className="project_title">Tunisian fann</h3>
+        <p className="project_paragraph">
+          If you’re confused about how to write your meta descriptions, you’re
+          not the only one. To help remove that confusion, I’m going to dig into
+          what makes a
+        </p>
+        <div className="project_button">
+          <button className="teck_button_relate">Demo </button>
+          <button className="teck_button_relate">Code</button>
         </div>
       </div>
     </div>
